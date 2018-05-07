6b2ca46e4a9f226ddfd85aa9d31a3ca40840e09a
jee <jag918@nyu.edu>
2018-05-07 23:23:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def take_a_number(katz_deli,name)
+  "Welcome,Ada. You are number #{katz_deli.size} in line."
+  katz_deli.push(name)
+end
+
+def line(katz_deli)
+  if katz_deli.size==0
+    puts "The line is currently empty."
+  else
+    counter=1
+    loop do 
+      puts "The line is currently:#{counter}. #{katz_deli[counter-1]},"
+      counter+=1
+      if counter>katz_deli.size
+        break
+      end
+    end
+  end
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.size==0
+    puts "The line is currently empty."
+  else
+    puts "Currently serving #{katzdeli.shift}."
+  end
+end
